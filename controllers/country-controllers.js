bce059d307fc84cf1242afd22a90bc71903b99a8
anaksingkong <wowo@raharja.info>
2021-02-24 04:47:43 +0700

    berhasil menampilkan semua country & country berdasarkan conde

--- controllers/country-controllers.js
+++ controllers/country-controllers.js
@@ -3,7 +3,32 @@
 const connection = require('../models/connection')
 const response = require('../resp/res')
 
+// tes routes untuk page home
 exports.index = ((req, res)=>{
     response.succsess("Belajar membuat rest api", res);
+});
+
+// get all country
+exports.allCountry = ((req, res)=>{
+    connection.query('SELECT (code) as id, (name) as value FROM country WHERE delete_flag = 0 AND inactive_flag = 0', (error, rows, fields)=>{
+        if (error) {
+            console.log(error);
+        } else {
+            response.succsess(rows, res);
+        }
+    });
+});
+
+// get country with code or name country
+exports.getCountryCode = ((req, res)=>{
+    var code = req.body.code;
+
+    connection.query('SELECT * FROM country WHERE code=? ', [code], (error, rows, fields)=>{
+        if (error) {
+            console.log(error)
+        } else {
+            response.succsess(rows, res)
+        }
+    });
 });
 
--- routes/country-routes.js
+++ routes/country-routes.js
@@ -7,5 +7,11 @@ module.exports = ((app)=>{
 
     app.route('/')
         .get(resJson.index);
-})
+
+    app.route('/api/v1/all-country')
+        .get(resJson.allCountry);
+
+    app.route('/api/v1/get-country-code/:code')
+        .get(resJson.getCountryCode);
+});
 
