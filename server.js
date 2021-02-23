ee31d1cc27700cbaafb8d5bcea86e1cc54d39183
anaksingkong <wowo@raharja.info>
2021-02-24 05:53:15 +0700

    run app

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // inisialisasi fungsi app pada route
 routes(app);
 
+// inisial port aplikasi berjalan
 app.listen(port, ()=>{
     console.log(`Server berjalan pada port ${port}`);
 });
